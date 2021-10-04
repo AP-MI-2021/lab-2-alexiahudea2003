43810654281b4156173b7c30a1779f799961af29
alexiahudea2003 <91561462+alexiahudea2003@users.noreply.github.com>
2021-10-04 17:16:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-Problema 1
+#Problema 1
 
 def get_largest_prime_below(n):
 	for i in range (n-1,1,-1):
@@ -13,11 +13,15 @@ def get_largest_prime_below(n):
 def test_get_largest_prime_below():
 	assert get_largesr_prime_below(8) == 7
 	assert get_largest_prime_below(12) == 11
- test_get_largest_prime_below()
+	assert get_largest_prime_below(30) == 29
+def main():
+	x=int(input("Care este numarul tau?)
+	print(get_largest_prime_below(x))
+if __name__ == '__main__':
+  main()
 
 
-
-Problema 2
+#Problema 2
 
 def is_palindrome(n):
 	y=n
@@ -33,4 +37,38 @@ def is_palindrome(n):
 def test_is_palindrome():
 	assert is_palindrome(76) == False
 	assert is_palindrome(77) == True
-test_is_palindrome()
+	assert is_palindrome(121) == True 
+def main():
+	x=int(input("Care este numarul tau?)
+	print(get_largest_prime_below(x))
+if __name__ == '__main__':
+  main()
+	
+		    
+#Problema 3
+		    
+def get_base_2(n):
+	if n==0
+		return "0"
+	else:
+		n=int(n)
+		baza=2
+		p=1
+		y=0
+		while n>=1:
+		    	m=n%2
+			y=y+m*p
+		   	n=n//2
+		    	p=p*10
+		return y
+def test_get_base_2():
+	assert get_base_2(4) == 100
+	assert get_base_2(5) == 101
+	assert get_base_2(0) == 0 
+def main():
+	x=int(input("Care este numarul tau?)
+	print(get_base_2(x))
+if __name__ == '__main__':
+  main()
+	
+
